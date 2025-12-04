2e51b4cbcb716636d538aeaa969a3169cf066a69
nora2605 <nora.ja2605@gmail.com>
2025-12-04 10:40:39 +0100

    day 2

---
+++ Day2.cs
@@ -0,0 +1,57 @@
+﻿using System.Numerics;
+using System.Text.Unicode;
+
+namespace AdventOfCode2025;
+
+public class Day2
+{
+    (BigInteger, BigInteger)[] ranges;
+
+    public Day2()
+    {
+        var input = new StreamReader("inputs/day2.txt")!.ReadToEnd();
+        ranges = [.. input.Split(',').Select(r => { var s = r.Split('-').Select(BigInteger.Parse).ToArray(); return (s[0], s[1]); })];
+    }
+
+    public BigInteger Part1()
+    {
+        BigInteger acc = 0;
+        foreach (var r in ranges)
+        {
+            for (BigInteger i = r.Item1; i <= r.Item2; i++)
+            {
+                string j = i.ToString();
+                if (j.Length % 2 == 1) continue;
+                if (j[..(j.Length / 2)] == j[(j.Length / 2)..])
+                    acc += i;
+            }
+        }
+        return acc;
+    }
+
+    public BigInteger Part2()
+    {
+        BigInteger acc = 0;
+        foreach (var r in ranges)
+        {
+            for (BigInteger i = r.Item1; i <= r.Item2; i++)
+            {
+                string j = i.ToString();
+                string pat = j[..1];
+                for (int m = 1; m <= j.Length / 2; m++)
+                {
+                    if (
+                        j.Length % pat.Length == 0 &&
+                        j[m..].Zip(pat).All(t => t.First == t.Second) &&
+                        string.Join("", Enumerable.Repeat(pat, j.Length / pat.Length)) == j
+                    ) {
+                        acc += i;
+                        break;
+                    }
+                    else pat += j[m];
+                }
+            }
+        }
+        return acc;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -13,6 +13,14 @@ t.Round("Day 1 - Init");
 Console.WriteLine(t.RoundWith("Day 1 - Star 1", d1.Part1));
 Console.WriteLine(t.RoundWith("Day 1 - Star 2", d1.Part2));
 
+// Day 1
+
+Day2 d2 = new();
+t.Round("Day 2 - Init");
+
+Console.WriteLine(t.RoundWith("Day 2 - Star 1", d2.Part1));
+Console.WriteLine(t.RoundWith("Day 2 - Star 2", d2.Part2));
+
 // Timing
 
 Console.WriteLine("\nTimes:");
